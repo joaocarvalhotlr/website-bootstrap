bab83ef9fbabb11a0c2876d707f72ae549db0037
João <randouplay@gmail.com>
2021-07-02 00:23:59 -0300

    boot

---
+++ js/main.js
@@ -0,0 +1,48 @@
+jQuery(document).ready(function($) {
+    window.onscroll = function() {
+        if(window.pageYOffset > 140){
+            $('#header').addClass('active')
+        }else {
+            $('#header').removeClass('active')
+        }
+    }
+
+    let btns = $('#servicos .button-group button')
+
+    btns.click(function(e) {
+        $('#servicos .button-group button').removeClass('active')
+        e.target.classList.add('active')
+
+        let selector = $(e.target).attr('data-filter')
+        $('#servicos .grid').isotope({
+            filter: selector,
+        })
+    })
+
+    $(window).on('load', function() {
+        $('#servicos .grid').isotope({
+            filter: "*"
+        })
+    })
+
+    $(".owl-carousel").owlCarousel({
+        loop: false,
+        margin: 30,
+        autoplay: true,
+        autoplayTimeout: 6000,
+        dots: true,
+        lazyLoad: true,
+        nav: false,
+        responsive: {
+          0: {
+            items: 1,
+          },
+          600: {
+            items: 1,
+          },
+          1000: {
+            items: 2,
+          },
+        },
+    })    
+})
